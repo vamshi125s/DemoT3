00945d3c7f2afa8365cf0ac2bb01eebd68b0e42f
vamshi125s <vamshi14908@gmail.com>
2025-01-10 09:39:15 +0530

    Pushing functions

---
+++ functions.js
@@ -0,0 +1,67 @@
+
+// function addTocart(){
+//     alert("Product added to cart")
+// }
+
+// function add(a,b){
+//     console.log(a+b)
+// }
+// add(10,20)
+// add(30,10)
+// add(50,25)
+// add(35,12)
+
+//functions without parameters
+// function demo(){
+//     console.log("Function without parameters");
+// }
+// demo();
+
+// Function with parameters
+
+// function userdetails(name,age,city){
+//     console.log(name);
+//     console.log(age);
+//     console.log(city);
+// }
+// userdetails("Vamshi",20,"Hyderabad");
+
+// Anonymus function example
+
+// function(){
+//     console.log("Anonymus function");
+// }
+// ();
+
+// Function expression
+// let x= function(){
+//     console.log("Function expression");
+// };
+// x();
+
+//IIFE
+
+// (function(){
+//     console.log("IIFE");
+// })
+// ();
+
+// Arrow function
+
+// let x =()=>{
+//     console.log("Arrow function");
+// }
+// x();
+
+// let x =a=>console.log(a);
+// x(5);
+
+// let x = (a,b)=>console.log(a+b);
+// x(5,8);
+
+let x=_=>{console.log("Hi");
+    console.log("Hello");
+    console.log("Bye");
+}
+x();
+
---
+++ loopings.js
@@ -0,0 +1,43 @@
+// // for loop
+
+// for(var i=1;i<=100;i++){
+//     console.log("Hello World");
+// }
+
+// for(var i=1;i<=100;i++){
+//     console.log(i);
+// }
+
+// for(var i=1;i<=100;i++){
+//     if(i%2==0){
+//         console.log(i+" is even number");
+//     } else {
+//         console.log(i+" is odd number");
+//     }
+// }
+
+// let i=1;
+// while(i<=5){
+//     console.log("Good Morning!!!");
+//     i++;
+// }
+
+// let i = 1;
+// let n = 5;
+// do{
+//     i++;
+//     console.log("batch 3 ")
+// }
+// while(i<=n)
+
+// for(let i=1; i<=10;i++){
+//     console.log(i);
+//     if(i==5)break
+// }
+
+// for(let i=1;i<=10;i++){
+//     if(i==3) continue
+//     console.log(i);
+// }
+
+
