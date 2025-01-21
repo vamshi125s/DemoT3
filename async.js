e3c2dd5760708ef2d7ef75b14ac4d5952ed23895
vamshi125s <vamshi14908@gmail.com>
2025-01-21 13:02:49 +0530

    Add files via upload

---
+++ async.js
@@ -0,0 +1,34 @@
+
+// console.log(10);
+// console.log(20);
+// // setTimeout(()=>{
+// //     console.log(30);
+// // },5000)
+// setInterval(()=>{
+//     //  document.writeln("MRU");
+//      console.lOg(30);
+// },1000)
+// console.log(40);
+// console.log(50);
+
+
+
+// promise :
+
+// let p1 =  new Promise((resolve,reject)=>{});
+// console.log(p1);
+
+// let p2 = new Promise((resolve,reject)=>{
+//     resolve("Success");
+// })
+// // console.log(p2);
+// p2.then((response)=> console.log(response))
+// p2.catch((error)=> console.log(error))
+// p2.finally(()=> console.log("Finally for both"))
+
+
+// let p3 = new Promise((resolve,reject)=>{
+//     reject("Failure");
+// })
+// console.log(p3);
+
