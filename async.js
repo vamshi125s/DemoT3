0b157f6f46b44f3621d3f5f40a1c78d00b63a450
vamshi125s <vamshi14908@gmail.com>
2025-01-23 13:06:23 +0530

    pusing async to git

--- async.js
+++ async.js
@@ -35,28 +35,60 @@
 
 // API fetching
 
-function fetchUsers(){
-    let response = fetch("https://jsonplaceholder.typicode.com/users");
-    // console.log(response);
-    response.then(result=>{
-        //console.log.json();
-        return result.json().then(data=>{
-            // console.log(data);
-            console.log(data);
-            let store = document.getElementById("store");
-            console.log(store);
-            data.map(user=>{
-                store.innerHTML +=`
-                <tr>
-                    <td>{user.id}</td>
-                    <td>{user.name}</td>
-                    <td>{user.email}</td>
-                    <td>{user.company.name}</td>
-                </tr>
-                `
-            })
-        })
-    })
-    .catch(err=>console.log(err))
+// function fetchUsers(){
+//     let response = fetch("https://jsonplaceholder.typicode.com/users");
+//     // console.log(response);
+//     response.then(result=>{
+//         //console.log.json();
+//         return result.json().then(data=>{
+//             // console.log(data);
+//             console.log(data);
+//             let store = document.getElementById("store");
+//             console.log(store);
+//             data.map(user=>{
+//                 store.innerHTML +=`
+//                 <tr>
+//                     <td>{user.id}</td>
+//                     <td>{user.name}</td>
+//                     <td>{user.email}</td>
+//                     <td>{user.company.name}</td>
+//                 </tr>
+//                 `
+//             })
+//         })
+//     })
+//     .catch(err=>console.log(err))
+// }
+// fetchUsers();
+
+//Async and await
+
+// function demo(){
+//     console.log("Start");
+//     console.log(10);
+//     console.log(20);
+//     console.log(30);
+//     console.log(40);
+//     console.log("End");
+// }
+// demo();
+
+let p = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve("Success");
+    },5000)
+})
+async function demo(){
+    console.log("Start");
+    let x = await p;
+    console.log(x);
+    console.log("End");
+};
+demo();
+
+async function fetchUsers(){
+    let response = await fetch("https://jsonplaceholder.typicode.com/users");
+    let data = await response.json();
+    console.log(data);
 }
 fetchUsers();
