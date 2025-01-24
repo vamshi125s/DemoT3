831c81696f35d06db7fda49b512618629816627d
vamshi125s <vamshi14908@gmail.com>
2025-01-24 12:54:54 +0530

    Pushing events

--- dom.js
+++ dom.js
@@ -1,1 +1,53 @@
-let 
+// console.log(window);
+// console.log(document);
+
+// let element = document.getElementById("demo");
+// element.innerText = "G+"
+// console.log(element);
+
+// let ele = document.getElementById("test");
+// // ele.innerText= "<h1>Header</h1>"
+// ele.innerHTML = "<h1>Header</h1>"
+// console.log(ele);
+
+// let ele = document.getElementsByClassName("test");
+// console.log(ele);
+// console.log(Array.isArray(ele));
+// ele[0].innerText="Hello"
+
+// //Spread operator :- it will takeout each and every value from the original array and sore it inside one more new array(pure array).
+
+// // syntax for Spread operator
+// let x = [...ele];
+// console.log(x, Array.isArray(x));
+// // console.log(x, Array.isArray(x));
+// x.map(element=>{
+//     // console.log(element);
+//     element.style.backgroundColor="orange"
+// })
+
+
+// let ele = document.getElementsByTagName("div");
+// console.log(ele);
+
+// let ele = document.getElementsByName("formData");
+
+// let ele = document.querySelector(".test");
+// console.log(ele);
+
+// let ele = document.querySelectorAll(".test");
+// console.log(ele);
+
+// let btn = document.querySelector("#btn");
+// btn.addEventListener("Click",()=>{
+//     // console.log("Button clicked");
+//     // alert("hi")
+//     // document.body.style.querySelector("#btn");
+
+// })
+
+// let btn = document.querySelector("#btn");
+// BtnInt.addeventListener("mouseover",()=>{
+//
+//
+// })
