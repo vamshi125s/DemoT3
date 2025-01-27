c8adaec9bd6e6f16467258f1b7aea0517751712b
vamshi125s <vamshi14908@gmail.com>
2025-01-27 12:55:34 +0530

    Pushing form

--- dom.js
+++ dom.js
@@ -24,7 +24,7 @@
 // x.map(element=>{
 //     // console.log(element);
 //     element.style.backgroundColor="orange"
-// })
+// // })
 
 
 // let ele = document.getElementsByTagName("div");
@@ -51,3 +51,43 @@
 //
 //
 // })
+
+// 
+let mainEle = document.createElement("div");
+mainEle.setAttribute("id","mainBlock");
+mainEle.style.border="2px solid red";
+mainEle.style.width="550px";
+mainEle.style.height="550px";
+
+console.log(mainEle);
+
+let topEle = document.createElement("div");
+topEle.setAttribute("class","topBlock");
+//console.log(topEle);
+
+let image = document.createElement("img");
+image.src="https://www.carandbike.com/_next/image?url=https%3A%2F%2Fimages.carandbike.com%2Fcms%2Farticles%2F2023%2F2%2F3205931%2FBMW_M340i_3_e1f5ea64dc.jpg&w=3840&q=75";
+image.style.width="550px"
+
+let bottomEle = document.createElement("div");
+bottomEle = setAttribute("class","bottomBlock");
+//console.log(bottomEle);
+
+let h1 = document.createElement("h1");
+h1.innerText = "CAT";
+h1.style.textAlign="center";
+
+let btn = document.createElement("button");
+btn.innerText="View More";
+btn.style.border="none";
+btn.style.padding="20px";
+btn.style.backgroundColor="dodgerblue";
+btn.style.color="white";
+
+bottomEle.appendChild(h1);
+bottomEle.appendChild(btn);
+
+topEle.appendChild(image);
+mainEle.appendChild(topEle);
+mainEle.appendChild(bottomEle);
+document.body.appendChild(mainEle);
